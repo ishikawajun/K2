778c9557cea37dc970f092e0af308005370294fc
ishikawajun <76640586+ishikawajun@users.noreply.github.com>
2024-03-09 16:23:17 +0900

    refactor: db config chenge from direct to env (#3)

--- backend/src/go/util.go
+++ backend/src/go/util.go
@@ -5,24 +5,40 @@ import (
 	"log"
 	"time"
 
+	"github.com/caarlos0/env/v10"
 	"github.com/go-sql-driver/mysql"
 )
 
+type dbConfig struct {
+	DBName               string `env:"DB_NAME"`
+	User                 string `env:"DB_USER"`
+	Passwd               string `env:"DB_PASSWORD"`
+	Addr                 string `env:"DB_ADDRESS"`
+	Net                  string `env:"DB_NET"`
+	ParseTime            bool   `env:"DB_PARSE_TIME"`
+	Collation            string `env:"DB_COLLATION"`
+	AllowNativePasswords bool   `env:"DB_ALLOW_NATIVE_PASSWOREDS"`
+}
+
 func connectDB() *sql.DB {
+	config := dbConfig{}
+	if err := env.Parse(&config); err != nil {
+		log.Printf("Failed convert env:%v", err)
+	}
 	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
-		log.Printf("Faild convart jst:%v", err)
+		log.Printf("Failed convert jst:%v", err)
 	}
 	c := mysql.Config{
-		DBName:               "bi",
-		User:                 "linux_server",
-		Passwd:               "Miruta728@",
-		Addr:                 "192.168.1.200:3306",
-		Net:                  "tcp",
-		ParseTime:            true,
-		Collation:            "utf8mb4_unicode_ci",
+		DBName:               config.DBName,
+		User:                 config.User,
+		Passwd:               config.Passwd,
+		Addr:                 config.Addr,
+		Net:                  config.Net,
+		ParseTime:            config.ParseTime,
+		Collation:            config.Collation,
 		Loc:                  jst,
-		AllowNativePasswords: true,
+		AllowNativePasswords: config.AllowNativePasswords,
 	}
 	db, err := sql.Open("mysql", c.FormatDSN())
 	if err != nil {
